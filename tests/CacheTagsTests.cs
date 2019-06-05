ce2be786c908b8997e4c8cfa3343c055d7c1a4ac
Cleyton Souza <souza.cleyton@gmail.com>
2019-06-05 19:16:31 -0300

    Adding first tests as templates

--- tests/CacheTagsTests.cs
+++ tests/CacheTagsTests.cs
@@ -1,25 +1,40 @@
-using FluentAssertions;    
+using FluentAssertions;
 using Xunit;
-using CentauroTech.Utils.CacheTags;
+
+
+using System.Web.Http.Controllers;
+using System.Linq;
+using System.Web.Http;
+using System.Net.Http;
+using System.Web.Http.Routing;
+using System.Web.Http.Hosting;
+using System.Net;
 using System.Collections.Generic;
-using System;
-using System.Collections.Specialized;
 
 namespace CentauroTech.Utils.CacheTags.Tests.UnitTests
-{    
+{
     public class CacheTagsTests
     {
-        [Fact]       
-        public void Returns_no_header_when_emptylist_is_passed()
+     
+
+        [Fact]
+        public void Returns_no_header_when_addcachetag_isfalse()
         {
-                
+            var controller = new CacheTaggedApiController();
+            var config = new HttpConfiguration();
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/api/test?query=123");
+            var route = config.Routes.MapHttpRoute("default", "api/{controller}");
+            var routeData = new HttpRouteData(route, new HttpRouteValueDictionary { { "controller", "test" } });
+
+            controller.ControllerContext = new HttpControllerContext(config, routeData, request);
+            controller.Request = request;
+            controller.Request.Properties[HttpPropertyKeys.HttpConfigurationKey] = config;
+         
+            var response =  request.CreateResponse(HttpStatusCode.OK, new List<string> { });
+
+            response.Headers.Any(x => x.Key == "Edge-Cache-Tag").Should().BeFalse();
+
         }
 
-        [Fact]       
-        public void Returns_header_when_noemptylist_is_passed()
-        {
-                    
-        }
-            
     }
 }
